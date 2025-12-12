1dc8a0ee4a3661e50210566394e6b779cae2e7a5
dhruvddverma-ai <dhruv.ddverma@gmail.com>
2025-12-12 16:07:26 +0530

    Add files via upload

---
+++ TASK1.py
@@ -0,0 +1,24 @@
+#Task 1: Perform Basic Mathematical Operations
+
+#1.  Takes two numbers as input from the user.
+#2.  Performs the basic mathematical operations on these two numbers:
+#	Addition
+#	Subtraction
+#	Multiplication
+#	Division
+
+#Excercise:
+
+"""""
+input1 = int(input("Enter first number: "))
+input2 = int(input("Enter second number: "))
+result=input1 + input2
+print("Addition: ", result)
+result=input1 - input2
+print("Subtraction: ", result)
+result=input1 * input2
+print("Multiplication: ", result)
+result=input1 / input2
+print("Division: ", result)
+
+"""
---
+++ TASK2.py
@@ -0,0 +1,13 @@
+#Task 2: Create a Personalized Greeting
+
+#1.  Takes a user's first name and last name as input.
+#2.  Concatenates the first name and last name into a full name.
+#3.  Prints a personalized greeting message using the full name.
+
+#Excercise:
+
+""""
+name = input("Enter your first name: ")
+surname = input("Enter your last name: ")
+full_name = name + " " + surname
+print("Hello, " + full_name + "! Welcome to the Python program.")
